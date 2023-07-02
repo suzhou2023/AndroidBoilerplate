b4b28cee85ccadb49fed9447fca9671f22c81074
suzhou <withwind318@gmail.com>
2023-07-02 21:20:16 +0800

    first commit

---
+++ app/src/main/java/com/bbt2000/androidboilerplate/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.bbt2000.androidboilerplate
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContent {
+
+        }
+    }
+}
+
+
+
+
+
