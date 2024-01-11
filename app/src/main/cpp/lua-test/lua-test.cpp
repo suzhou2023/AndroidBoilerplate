46a93961ffda1c4709331f5ca06bc2c7ca7f6262
suzhou <suzhou2023r@gmail.com>
2024-01-11 19:31:06 +0800

    添加lua-5.4.6源码进行编译

---
+++ app/src/main/cpp/lua-test/lua-test.cpp
@@ -0,0 +1,8 @@
+/**
+ *  author : sz
+ *  date : 2024/1/11
+ *  description : 
+ */
+
+
+#include "lua-test.h"
---
+++ app/src/main/cpp/lua-test/lua-test.h
@@ -0,0 +1,11 @@
+/**
+ *  author : sz
+ *  date : 2024/1/11
+ *  description : 
+ */
+
+
+#ifndef ANDROIDBOILERPLATE_LUA_TEST_H
+#define ANDROIDBOILERPLATE_LUA_TEST_H
+
+#endif //ANDROIDBOILERPLATE_LUA_TEST_H
